a252cd859893049a59de061af648daea9fc27318
sebastian7010 <sebastianssc01@gmail.com>
2024-08-23 22:21:18 -0500

    cambios en el about y formulario de reserva

--- src/app/components/card-service/card-service.component.ts
+++ src/app/components/card-service/card-service.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-card-service',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './card-service.component.html',
   styleUrl: './card-service.component.css'
 })
